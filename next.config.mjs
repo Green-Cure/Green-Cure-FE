7e47ba59a2cb8014235c07820f118404e0fa3e01
RizkyZaki <ifsry416@gmail.com>
2024-07-17 13:06:07 +0700

    chore: Skip GithubPages

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  // TODO : komen output, assetPrefix & basePath jika running lokal, aktifkan kembali ketika push
-  output: "export",
-  basePath: "/Green-Cure-FE",
-};
+const nextConfig = {};
 
 export default nextConfig;
