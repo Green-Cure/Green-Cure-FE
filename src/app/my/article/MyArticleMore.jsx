540699337e7a0943f4df4e3a8dd4a436f3f61fb8
mhndkptr <muhamadhendika77@gmail.com>
2024-07-23 10:11:38 +0700

    Add my article page

---
+++ src/app/my/article/MyArticleMore.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function MyArticleMore({ articles }) {
+  return (
+    <>
+      <div className="sm:w-3/5 w-full sm:mt-0 mt-1">
+        <h1 className="gcHeading3p text-gcPrimary-1000 sm:hidden">More</h1>
+        {articles.map((article, index) => {
+          return (
+            <div key={index} className="border-b sm:p-5 py-3 border-gcSecondary-300">
+              <Link href={`/my/article/${article.slug}`} className="hover:underline text-gcPrimary-1000">
+                <h1 className="gcBody1p font-semibold text-gcPrimary-1000">{article.title}</h1>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
---
+++ src/app/my/article/page.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import MyArticleSlider from "./MyArticleSlider";
+import { useContext } from "react";
+import { ArticleContext } from "./layout";
+import MyArticleMore from "./MyArticleMore";
+import MyArticleTrending from "./MyArticleTrending";
+
+export default function MyArticle() {
+  const { articles, loading, error, meta } = useContext(ArticleContext);
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    const month = months[date.getUTCMonth()];
+    const day = date.getUTCDate();
+    const year = date.getUTCFullYear();
+
+    return `${month} ${day}, ${year}`;
+  };
+
+  const getLatestArticles = (data, count) => {
+    return data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).slice(0, count);
+  };
+
+  const getRandomArticles = (data, count) => {
+    return data.sort(() => 0.5 - Math.random()).slice(0, count);
+  };
+
+  const trending = getLatestArticles(articles, 3);
+  const slides = getRandomArticles(articles, 3);
+
+  return (
+    <>
+      <MyArticleSlider slides={slides} autoSlideInterval={3000} />
+      <div className="flex lg:mt-16 md:mt-12 sm:mt-10 mt-6 xl:gap-20 lg:gap-16 md:gap-14 sm:gap-10 gap-6 mx-4 sm:flex-row flex-col-reverse">
+        <MyArticleMore articles={articles} />
+        <MyArticleTrending trending={trending} formatDate={formatDate} />
+      </div>
+    </>
+  );
+}
