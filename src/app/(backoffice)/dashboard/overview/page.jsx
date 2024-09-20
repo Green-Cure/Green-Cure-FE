4c3c85ed7b71821b4a60ff5288ba90606347ddd9
mhndkptr <muhamadhendika77@gmail.com>
2024-09-21 00:48:17 +0700

    chore: finish pagination in article backoffice

--- src/app/(backoffice)/dashboard/overview/page.jsx
+++ src/app/(backoffice)/dashboard/overview/page.jsx
@@ -2,21 +2,21 @@ export default function DashboardOverview() {
   return (
     <>
       <div className="grid grid-cols-3 gap-4 mb-4">
-        <div className="flex items-center justify-center h-24 rounded bg-gray-200">
+        <div className="flex items-center justify-center h-44 rounded bg-gcPrimary-1000">
           <p className="text-2xl text-gray-400">
             <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
             </svg>
           </p>
         </div>
-        <div className="flex items-center justify-center h-24 rounded bg-gray-200">
+        <div className="flex items-center justify-center h-44 rounded bg-gcPrimary-1000">
           <p className="text-2xl text-gray-400">
             <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
             </svg>
           </p>
         </div>
-        <div className="flex items-center justify-center h-24 rounded bg-gray-200">
+        <div className="flex items-center justify-center h-44 rounded bg-gcPrimary-1000">
           <p className="text-2xl text-gray-400">
             <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
@@ -24,6 +24,7 @@ export default function DashboardOverview() {
           </p>
         </div>
       </div>
+
       <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-200">
         <p className="text-2xl text-gray-400">
           <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
@@ -31,6 +32,7 @@ export default function DashboardOverview() {
           </svg>
         </p>
       </div>
+
       <div className="grid grid-cols-2 gap-4 mb-4">
         <div className="flex items-center justify-center rounded bg-gray-200 h-28">
           <p className="text-2xl text-gray-400">
@@ -61,6 +63,7 @@ export default function DashboardOverview() {
           </p>
         </div>
       </div>
+
       <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-200">
         <p className="text-2xl text-gray-400">
           <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
@@ -68,6 +71,7 @@ export default function DashboardOverview() {
           </svg>
         </p>
       </div>
+
       <div className="grid grid-cols-2 gap-4">
         <div className="flex items-center justify-center rounded bg-gray-200 h-28">
           <p className="text-2xl text-gray-400">
