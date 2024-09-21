89e025b80c3be703c1fb7a4bafadf61e708e2d61
mhndkptr <muhamadhendika77@gmail.com>
2024-09-22 01:18:52 +0700

    chore: finish dashboard overview

--- src/app/(backoffice)/dashboard/overview/page.jsx
+++ src/app/(backoffice)/dashboard/overview/page.jsx
@@ -3,11 +3,14 @@
 import request from "@/app/utils/request";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import OverviewUsersChart from "./OverviewUsersChart";
 
 export default function DashboardOverview() {
   const [articleMeta, setArticleMeta] = useState(null);
   const [plantMeta, setPlantMeta] = useState(null);
   const [plantDiseasesMeta, setPlantDiseasesMeta] = useState(null);
+  const [forum, setForum] = useState(null);
+  const [forumReport, setForumReport] = useState(null);
   const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -17,10 +20,7 @@ export default function DashboardOverview() {
     request
       .get(`articles?page=1&limit=0`)
       .then(function (response) {
-        if (
-          response.data?.statusCode === 200 ||
-          response.data?.statusCode === 201
-        ) {
+        if (response.data?.statusCode === 200 || response.data?.statusCode === 201) {
           if (response.data.meta) {
             setArticleMeta(response.data.meta);
           } else {
@@ -45,10 +45,7 @@ export default function DashboardOverview() {
     request
       .get("plants?page=1&limit=0")
       .then(function (response) {
-        if (
-          response.data?.statusCode === 200 ||
-          response.data?.statusCode === 201
-        ) {
+        if (response.data?.statusCode === 200 || response.data?.statusCode === 201) {
           if (response.data.meta) {
             setPlantMeta(response.data.meta);
           } else {
@@ -70,10 +67,7 @@ export default function DashboardOverview() {
       .get("plant-diseases?page=1&limit=0")
       .then(function (response) {
         if (response.data) {
-          if (
-            response.data?.statusCode === 200 ||
-            response.data?.statusCode === 201
-          ) {
+          if (response.data?.statusCode === 200 || response.data?.statusCode === 201) {
             if (response.data.meta) {
               setPlantDiseasesMeta(response.data.meta);
             } else {
@@ -99,10 +93,7 @@ export default function DashboardOverview() {
     request
       .get("user")
       .then(function (response) {
-        if (
-          response.data?.statusCode === 200 ||
-          response.data?.statusCode === 201
-        ) {
+        if (response.data?.statusCode === 200 || response.data?.statusCode === 201) {
           if (response.data.data.length > 0) {
             setUsers(response.data.data);
           } else {
@@ -123,13 +114,56 @@ export default function DashboardOverview() {
         toast.dismiss();
         toast.error("An unexpected error occurred");
       });
+
+    request
+      .get("forum")
+      .then(function (response) {
+        if (response.data?.statusCode === 200 || response.data?.statusCode === 201) {
+          if (response.data.data.length > 0) {
+            setForum(response.data.data);
+          } else {
+            setForum(null);
+          }
+        } else if (response.data.statusCode === 500) {
+          console.error("INTERNAL_SERVER_ERROR");
+          toast.error("Server Error");
+        } else {
+          toast.error("An unexpected error occurred");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        toast.error("An unexpected error occurred");
+      });
+
+    request
+      .get("report")
+      .then(function (response) {
+        if (response.data?.statusCode === 200 || response.data?.statusCode === 201) {
+          if (response.data.data.length > 0) {
+            setForumReport(response.data.data);
+          } else {
+            setForumReport([]);
+          }
+        } else if (response.data?.statusCode === 500) {
+          console.error("INTERNAL_SERVER_ERROR");
+          toast.dismiss();
+          toast.error("Server Error");
+        } else {
+          toast.error("An unexpected error occurred");
+        }
+      })
+      .catch((err) => {
+        toast.error("An unexpected error occurred");
+        console.error(err);
+      });
   }, []);
 
   useEffect(() => {
-    if (articleMeta && plantMeta && plantDiseasesMeta && users) {
+    if (articleMeta && plantMeta && plantDiseasesMeta && users && forum) {
       setIsLoading(false);
     }
-  }, [articleMeta, plantMeta, plantDiseasesMeta, users]);
+  }, [articleMeta, plantMeta, plantDiseasesMeta, users, forum]);
 
   return (
     <>
@@ -145,91 +179,44 @@ export default function DashboardOverview() {
         </>
       ) : (
         <>
-          <div className="grid grid-cols-3 gap-4 mb-4">
+          <div className="grid grid-cols-3 lg:gap-4 md:gap-3 sm:gap-2.5 gap-2 lg:mb-4 md:mb-3 sm:mb-2.5 mb-2">
             <div className="flex flex-col justify-center xl:h-40 lg:h-36 md:h-32 sm:h-28 h-24 rounded bg-gcPrimary-1000 lg:p-5 md:p-4 sm:p-3 p-2.5">
-              <p className="place-self-start gcContentBody1p text-gcNeutrals-baseWhite">
-                Article Uploaded
-              </p>
-              <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite place-self-center md:mt-2 mt-1">
-                {articleMeta?.total}
-              </h3>
+              <p className="place-self-start gcContentBody1p text-gcNeutrals-baseWhite">Article Uploaded</p>
+              <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite place-self-center md:mt-2 mt-1">{articleMeta?.total}</h3>
             </div>
             <div className="flex flex-col justify-center xl:h-40 lg:h-36 md:h-32 sm:h-28 h-24 rounded bg-gcPrimary-1000 lg:p-5 md:p-4 sm:p-3 p-2.5">
-              <p className="place-self-start gcContentBody1p text-gcNeutrals-baseWhite">
-                Plant Uploaded
-              </p>
-              <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite place-self-center md:mt-2 mt-1">
-                {plantMeta?.total}
-              </h3>
+              <p className="place-self-start gcContentBody1p text-gcNeutrals-baseWhite">Plant Uploaded</p>
+              <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite place-self-center md:mt-2 mt-1">{plantMeta?.total}</h3>
             </div>
             <div className="flex flex-col justify-center xl:h-40 lg:h-36 md:h-32 sm:h-28 h-24 rounded bg-gcPrimary-1000 lg:p-5 md:p-4 sm:p-3 p-2.5">
-              <p className="place-self-start gcContentBody1p text-gcNeutrals-baseWhite">
-                Plant Diseases Uploaded
-              </p>
-              <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite place-self-center md:mt-2 mt-1">
-                {plantDiseasesMeta?.total}
-              </h3>
+              <p className="place-self-start gcContentBody1p text-gcNeutrals-baseWhite">Plant Diseases Uploaded</p>
+              <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite place-self-center md:mt-2 mt-1">{plantDiseasesMeta?.total}</h3>
             </div>
           </div>
 
-          {/* <div className="grid md:grid-cols-2 grid-cols-1 gap-4 mb-4">
-            <div className="flex items-center flex-col justify-center h-96 rounded bg-gcPrimary-1000 lg:p-5 md:p-4 sm:p-3 p-2.5 md:gap-2.5 gap-1.5">
+          <div className="grid lg:grid-cols-4 grid-cols-1 lg:gap-4 lg:mb-4 md:mb-3 sm:mb-2.5 mb-2">
+            <div className="col-span-3 flex items-center flex-col justify-center lg:h-96 md:h-80 sm:h-72 h-64 rounded bg-gcPrimary-1000 lg:p-5 md:p-4 sm:p-3 p-2.5 md:gap-2.5 gap-1.5">
               <div className="flex items-center justify-between w-full">
                 <div className="flex flex-row items-center justify-center md:gap-2.5 gap-1.5">
-                  <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite md:mt-2 mt-1">
-                    {users?.length}
-                  </h3>
-                  <p className="gcContentBody1p text-gcNeutrals-baseWhite">
-                    Active Users
-                  </p>
+                  <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite md:mt-2 mt-1">{users?.length}</h3>
+                  <p className="gcContentBody1p text-gcNeutrals-baseWhite">Active Users</p>
                 </div>
-                <p className="gcContentBody2p text-gcNeutrals-baseWhite">
-                  Last 6 Months
-                </p>
+                <p className="gcContentBody2p text-gcNeutrals-baseWhite">Last 6 Months</p>
               </div>
               {users && <OverviewUsersChart users={users} />}
             </div>
-            <div className="grid grid-cols-1 h-96 md:gap-2.5 gap-1.5">
-              <div className="flex items-center justify-center rounded bg-gcPrimary-1000">
-                <p className="text-2xl text-gray-400">
-                  <svg
-                    className="w-3.5 h-3.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 18 18"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 1v16M1 9h16"
-                    />
-                  </svg>
-                </p>
+
+            <div className="grid lg:grid-cols-1 grid-cols-2 lg:h-96 lg:gap-4 md:gap-3 sm:gap-2.5 gap-2 lg:mt-0 md:mt-3 sm:mt-2.5 mt-2">
+              <div className="flex flex-col justify-center rounded bg-gcPrimary-1000 lg:h-auto md:h-32 sm:h-28 h-24 lg:p-5 md:p-4 sm:p-3 p-2.5">
+                <p className="place-self-start gcContentBody1p text-gcNeutrals-baseWhite">Forum Posts</p>
+                <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite place-self-center md:mt-2 mt-1">{forum?.length}</h3>
               </div>
-              <div className="flex items-center justify-center rounded bg-gcPrimary-1000">
-                <p className="text-2xl text-gray-400">
-                  <svg
-                    className="w-3.5 h-3.5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 18 18"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 1v16M1 9h16"
-                    />
-                  </svg>
-                </p>
+              <div className="flex flex-col justify-center rounded bg-gcPrimary-1000 lg:h-auto md:h-32 sm:h-28 h-24 lg:p-5 md:p-4 sm:p-3 p-2.5">
+                <p className="place-self-start gcContentBody1p text-gcNeutrals-baseWhite">Forum Report Posts</p>
+                <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite place-self-center md:mt-2 mt-1">{forumReport?.length}</h3>
               </div>
             </div>
-          </div> */}
+          </div>
         </>
       )}
     </>
