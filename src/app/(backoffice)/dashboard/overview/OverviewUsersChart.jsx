6b50a8a68d2fabbf658dea9a3e7f51413efd9af7
mhndkptr <muhamadhendika77@gmail.com>
2024-09-21 20:26:16 +0700

    chore: update overvieww daashboard

---
+++ src/app/(backoffice)/dashboard/overview/OverviewUsersChart.jsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+
+export default function OverviewUsersChart({ users }) {
+  const [chartData, setChartData] = useState({});
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const totalAccountsByMonth = calculateTotalAccounts(users, 6);
+      const recentMonthsArray = generateRecentMonths(6);
+      setChartData({
+        options: {
+          chart: {
+            id: "greencure-users-report",
+          },
+          xaxis: {
+            categories: recentMonthsArray,
+          },
+        },
+        series: [
+          {
+            name: "Total Users",
+            data: totalAccountsByMonth,
+          },
+        ],
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      {users && chartData && <div className="chart bg-white rounded-md w-full h-full">{chartData && chartData.series && chartData.options && <Chart options={chartData.options} series={chartData.series} type="area" height={"100%"} />}</div>}
+    </>
+  );
+}
+
+function generateRecentMonths(monthCount) {
+  const today = new Date();
+  const result = [];
+
+  for (let i = 0; i < monthCount; i++) {
+    let currentMonth = today.getMonth() - i;
+    let currentYear = today.getFullYear();
+
+    // Handle cases where we need to wrap around to the previous year
+    if (currentMonth < 0) {
+      currentMonth += 12;
+      currentYear -= 1;
+    }
+
+    // Get the full month name (e.g., "January")
+    const monthName = new Intl.DateTimeFormat("en-US", { month: "long" }).format(new Date(currentYear, currentMonth, 1));
+
+    // Combine month name and year
+    const formattedDate = `${monthName}, ${currentYear}`;
+    result.unshift(formattedDate); // Add to the beginning of the array
+  }
+
+  return result;
+}
+
+function calculateTotalAccounts(data, monthCount) {
+  // Grouping data base on month
+  const groupedData = {};
+  data.forEach((account) => {
+    const createdAt = new Date(account.createdAt);
+    const monthYearKey = `${createdAt.getMonth()}-${createdAt.getFullYear()}`;
+    if (!groupedData[monthYearKey]) {
+      groupedData[monthYearKey] = 0;
+    }
+    groupedData[monthYearKey]++;
+  });
+
+  // Count total account for current month and previous month
+  const currentDate = new Date();
+  const result = [];
+  for (let i = monthCount - 1; i >= 0; i--) {
+    const targetDate = new Date(currentDate);
+    targetDate.setMonth(targetDate.getMonth() - i);
+    const targetMonthYearKey = `${targetDate.getMonth()}-${targetDate.getFullYear()}`;
+    const currentMonthAccounts = groupedData[targetMonthYearKey] || 0;
+
+    // Calculate previous month
+    const prevMonthDate = new Date(targetDate);
+    prevMonthDate.setMonth(prevMonthDate.getMonth() - 1);
+    const prevMonthYearKey = `${prevMonthDate.getMonth()}-${prevMonthDate.getFullYear()}`;
+    const prevMonthAccounts = groupedData[prevMonthYearKey] || 0;
+
+    // Combine current and previous month accounts
+    const totalAccounts = currentMonthAccounts + prevMonthAccounts;
+    result.push(totalAccounts);
+  }
+
+  return result;
+}
--- src/app/(backoffice)/dashboard/overview/page.jsx
+++ src/app/(backoffice)/dashboard/overview/page.jsx
@@ -2,11 +2,14 @@
 
 import request from "@/app/utils/request";
 import { useEffect, useState } from "react";
+import OverviewUsersChart from "./OverviewUsersChart";
+import toast from "react-hot-toast";
 
 export default function DashboardOverview() {
   const [articleMeta, setArticleMeta] = useState(null);
   const [plantMeta, setPlantMeta] = useState(null);
   const [plantDiseasesMeta, setPlantDiseasesMeta] = useState(null);
+  const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -84,69 +87,98 @@ export default function DashboardOverview() {
         toast.error("Server Error");
         return;
       });
+
+    request
+      .get("user")
+      .then(function (response) {
+        if (response.data?.statusCode === 200 || response.data?.statusCode === 201) {
+          if (response.data.data.length > 0) {
+            setUsers(response.data.data);
+          } else {
+            setUsers([]);
+          }
+          toast.dismiss();
+        } else if (response.data.statusCode === 500) {
+          console.error("INTERNAL_SERVER_ERROR");
+          toast.dismiss();
+          toast.error("Server Error");
+        } else {
+          toast.dismiss();
+          toast.error("An unexpected error occurred");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        toast.dismiss();
+        toast.error("An unexpected error occurred");
+      });
   }, []);
 
   useEffect(() => {
-    if (articleMeta && plantMeta && plantDiseasesMeta) {
+    if (articleMeta && plantMeta && plantDiseasesMeta && users) {
       setIsLoading(false);
     }
-  }, [articleMeta, plantMeta, plantDiseasesMeta]);
+  }, [articleMeta, plantMeta, plantDiseasesMeta, users]);
 
   return (
     <>
-      <div className="grid grid-cols-3 gap-4 mb-4">
-        <div className="flex flex-col justify-center xl:h-40 lg:h-36 md:h-32 sm:h-28 h-24 rounded bg-gcPrimary-1000 lg:p-5 md:p-4 sm:p-3 p-2.5">
-          <p className="place-self-start gcContentBody1p text-gcNeutrals-baseWhite">Article Uploaded</p>
-          <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite place-self-center md:mt-2 mt-1">{articleMeta?.total}</h3>
-        </div>
-        <div className="flex flex-col justify-center xl:h-40 lg:h-36 md:h-32 sm:h-28 h-24 rounded bg-gcPrimary-1000 lg:p-5 md:p-4 sm:p-3 p-2.5">
-          <p className="place-self-start gcContentBody1p text-gcNeutrals-baseWhite">Plant Uploaded</p>
-          <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite place-self-center md:mt-2 mt-1">{plantMeta?.total}</h3>
-        </div>
-        <div className="flex flex-col justify-center xl:h-40 lg:h-36 md:h-32 sm:h-28 h-24 rounded bg-gcPrimary-1000 lg:p-5 md:p-4 sm:p-3 p-2.5">
-          <p className="place-self-start gcContentBody1p text-gcNeutrals-baseWhite">Plant Diseases Uploaded</p>
-          <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite place-self-center md:mt-2 mt-1">{plantDiseasesMeta?.total}</h3>
-        </div>
-      </div>
+      {isLoading ? (
+        <>
+          <div className="flex justify-center items-center h-screen">
+            <div className="flex flex-row gap-2">
+              <div className="w-2.5 h-2.5 lg:w-4 lg:h-4 rounded-full bg-gcPrimary-basePrimary animate-bounce"></div>
+              <div className="w-2.5 h-2.5 lg:w-4 lg:h-4 rounded-full bg-gcPrimary-basePrimary animate-bounce [animation-delay:-.3s]"></div>
+              <div className="w-2.5 h-2.5 lg:w-4 lg:h-4 rounded-full bg-gcPrimary-basePrimary animate-bounce [animation-delay:-.5s]"></div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="grid grid-cols-3 gap-4 mb-4">
+            <div className="flex flex-col justify-center xl:h-40 lg:h-36 md:h-32 sm:h-28 h-24 rounded bg-gcPrimary-1000 lg:p-5 md:p-4 sm:p-3 p-2.5">
+              <p className="place-self-start gcContentBody1p text-gcNeutrals-baseWhite">Article Uploaded</p>
+              <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite place-self-center md:mt-2 mt-1">{articleMeta?.total}</h3>
+            </div>
+            <div className="flex flex-col justify-center xl:h-40 lg:h-36 md:h-32 sm:h-28 h-24 rounded bg-gcPrimary-1000 lg:p-5 md:p-4 sm:p-3 p-2.5">
+              <p className="place-self-start gcContentBody1p text-gcNeutrals-baseWhite">Plant Uploaded</p>
+              <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite place-self-center md:mt-2 mt-1">{plantMeta?.total}</h3>
+            </div>
+            <div className="flex flex-col justify-center xl:h-40 lg:h-36 md:h-32 sm:h-28 h-24 rounded bg-gcPrimary-1000 lg:p-5 md:p-4 sm:p-3 p-2.5">
+              <p className="place-self-start gcContentBody1p text-gcNeutrals-baseWhite">Plant Diseases Uploaded</p>
+              <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite place-self-center md:mt-2 mt-1">{plantDiseasesMeta?.total}</h3>
+            </div>
+          </div>
 
-      <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-200">
-        <p className="text-2xl text-gray-400">
-          <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-          </svg>
-        </p>
-      </div>
-
-      <div className="grid grid-cols-2 gap-4 mb-4">
-        <div className="flex items-center justify-center rounded bg-gray-200 h-28">
-          <p className="text-2xl text-gray-400">
-            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-            </svg>
-          </p>
-        </div>
-        <div className="flex items-center justify-center rounded bg-gray-200 h-28">
-          <p className="text-2xl text-gray-400">
-            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-            </svg>
-          </p>
-        </div>
-        <div className="flex items-center justify-center rounded bg-gray-200 h-28">
-          <p className="text-2xl text-gray-400">
-            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-            </svg>
-          </p>
-        </div>
-        <div className="flex items-center justify-center rounded bg-gray-200 h-28">
-          <p className="text-2xl text-gray-400">
-            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
-            </svg>
-          </p>
-        </div>
-      </div>
+          <div className="grid md:grid-cols-2 grid-cols-1 gap-4 mb-4">
+            <div className="flex items-center flex-col justify-center h-96 rounded bg-gcPrimary-1000 lg:p-5 md:p-4 sm:p-3 p-2.5 md:gap-2.5 gap-1.5">
+              <div className="flex items-center justify-between w-full">
+                <div className="flex flex-row items-center justify-center md:gap-2.5 gap-1.5">
+                  <h3 className="text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold gcDropShadow text-gcNeutrals-baseWhite md:mt-2 mt-1">{users?.length}</h3>
+                  <p className="gcContentBody1p text-gcNeutrals-baseWhite">Active Users</p>
+                </div>
+                <p className="gcContentBody2p text-gcNeutrals-baseWhite">Last 6 Months</p>
+              </div>
+              {users && <OverviewUsersChart users={users} />}
+            </div>
+            <div className="grid grid-cols-1 h-96 md:gap-2.5 gap-1.5">
+              <div className="flex items-center justify-center rounded bg-gcPrimary-1000">
+                <p className="text-2xl text-gray-400">
+                  <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
+                  </svg>
+                </p>
+              </div>
+              <div className="flex items-center justify-center rounded bg-gcPrimary-1000">
+                <p className="text-2xl text-gray-400">
+                  <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
+                  </svg>
+                </p>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
